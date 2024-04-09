59f7a8af526c0c8391c7077e00ea7a7bcd149431
p-w-g <p.grzelczyk@gmail.com>
2024-04-09 21:54:43 +0200

    scaffold list

---
+++ src/components/NoteList.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="q-pa-md">
+    <q-list bordered separator>
+      <q-slide-item @left="onLeft" @right="onRight">
+        <template v-slot:left>
+          <q-icon name="done" />
+        </template>
+        <template v-slot:right>
+          <q-icon name="mode_edit" />
+        </template>
+        <q-item>
+          <q-item-section>Task 2</q-item-section>
+        </q-item>
+      </q-slide-item>
+
+      <q-slide-item @left="onLeft" @right="onRight">
+        <template v-slot:left>
+          <q-icon name="done" />
+        </template>
+        <template v-slot:right>
+          <q-icon name="mode_edit" />
+        </template>
+        <q-item>
+          <q-item-section>Task 3</q-item-section>
+        </q-item>
+      </q-slide-item>
+
+      <q-slide-item @left="onLeft" @right="onRight">
+        <template v-slot:left>
+          <q-icon name="done" />
+        </template>
+        <template v-slot:right>
+          <q-icon name="mode_edit" />
+        </template>
+        <q-item>
+          <q-item-section>Task 1</q-item-section>
+        </q-item>
+      </q-slide-item>
+    </q-list>
+  </div>
+</template>
+
+<script>
+import { useQuasar } from "quasar";
+import { onBeforeUnmount } from "vue";
+
+export default {
+  setup() {
+    const $q = useQuasar();
+    let timer;
+
+    function finalize(reset) {
+      timer = setTimeout(() => {
+        reset();
+      }, 1000);
+    }
+
+    onBeforeUnmount(() => {
+      clearTimeout(timer);
+    });
+
+    return {
+      onLeft({ reset }) {
+        // TODO: implement action
+        finalize(reset);
+      },
+
+      onRight({ reset }) {
+        // TODO: implement action
+        finalize(reset);
+      },
+    };
+  },
+};
+</script>
