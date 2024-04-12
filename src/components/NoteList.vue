8ed2b64f3c65655dabc1fa288aad149983aec33c
p-w-g <p.grzelczyk@gmail.com>
2024-04-12 18:34:19 +0200

    refactor and implementation of removeNote function with store action

--- src/components/NoteList.vue
+++ src/components/NoteList.vue
@@ -4,8 +4,9 @@
       <q-slide-item
         v-for="note in notesList"
         v-bind:key="note.id"
-        @left="onLeft"
-        @right="onRight"
+        :id="note.id"
+        @left="(e) => removeNote({ reset: e.reset, id: note.id })"
+        @right="editNote"
       >
         <template v-slot:left>
           <q-icon name="done" />
@@ -22,40 +23,27 @@
 </template>
 
 <script>
-import { useQuasar } from "quasar";
-import { onBeforeUnmount } from "vue";
 import { storeToRefs } from "pinia";
 import { useNotesStore } from "../store.ts";
 
 export default {
   setup() {
-    const $q = useQuasar();
     const { notesList } = storeToRefs(useNotesStore());
-
-    let timer;
-
-    function finalize(reset) {
-      timer = setTimeout(() => {
-        reset();
-      }, 1000);
-    }
-
-    onBeforeUnmount(() => {
-      clearTimeout(timer);
-    });
+    const notesStore = useNotesStore();
 
     return {
-      onLeft({ reset }) {
-        // TODO: implement action
-        finalize(reset);
+      removeNote({ reset, id }) {
+        reset;
+        notesStore.removeNote(id);
       },
 
-      onRight({ reset }) {
+      editNote({ reset }) {
         // TODO: implement action
-        finalize(reset);
+        reset;
       },
 
       notesList,
+      notesStore,
     };
   },
 };
