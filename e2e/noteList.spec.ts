06827f772cb1806776cf61d9182f658ad40cfff3
p-w-g <p.grzelczyk@gmail.com>
2024-04-12 18:41:35 +0200

    add test case for deleting a task

--- e2e/noteList.spec.ts
+++ e2e/noteList.spec.ts
@@ -17,15 +17,27 @@ test("CRUD operations", async ({ page }) => {
   await page.getByRole("textbox").fill("Third Task");
   await page.getByText("Reset").click();
 
+  const task_1 = page.getByText("First Task");
+  const task_2 = page.getByText("Second Task");
+  const task_3 = page.getByText("Third Task");
+
   // find only two tasks
-  await expect(page.getByText("First Task")).toBeVisible();
-  await expect(page.getByText("Second Task")).toBeVisible();
-  await expect(page.getByText("Third Task")).not.toBeVisible();
+  await expect(task_1).toBeVisible();
+  await expect(task_2).toBeVisible();
+  await expect(task_3).not.toBeVisible();
 
   // delete first task
-  // edit second task
 
+  await task_1.dragTo(task_1, {
+    sourcePosition: { x: 0, y: 0 },
+    targetPosition: { x: 150, y: 0 },
+  });
+  await expect(task_1).not.toBeVisible();
+
+  // edit second task
   // only second task remains with the text "third task failed successfully"
+  // await expect(task_2).not.toBeVisible();
+  // await expect(page.getByText("Third task failed successfully")).toBeVisible();
   // clear button deletes everything
 
   // add a task with text "Fifth task" and it should be with ID 0
