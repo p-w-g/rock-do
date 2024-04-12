959c146088589f2fb6579701348992c766be0910
p-w-g <p.grzelczyk@gmail.com>
2024-04-12 17:13:40 +0200

    remove boilerplate

--- src/store.ts
+++ src/store.ts
@@ -3,15 +3,7 @@ import { Note } from "./interfaces";
 
 export const useNotesStore = defineStore("notes", {
   state: () => ({
-    _notes: [
-      {
-        user: "first user",
-        description: "first task",
-        id: -1,
-        anonymous: false,
-        isPrivate: false,
-      },
-    ] as Note[],
+    _notes: [] as Note[],
     id: 0,
   }),
   getters: {
