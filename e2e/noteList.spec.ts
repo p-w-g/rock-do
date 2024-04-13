229a786bff87e1971f0adac079226c2fe56b894e
p-w-g <p.grzelczyk@gmail.com>
2024-04-13 20:09:08 +0200

    bulletproofing test

--- e2e/noteList.spec.ts
+++ e2e/noteList.spec.ts
@@ -55,7 +55,55 @@ test("CRUD operations", async ({ page }) => {
     .click();
 
   await expect(task_2).not.toBeVisible();
-  await expect(page.getByText("Third task failed successfully")).toBeVisible();
 
-  // clear button deletes everything
+  // adding a note is possible using enter
+  await page.getByRole("textbox").fill("Fourth Task");
+  await page.keyboard.press("Enter");
+
+  // adding a note is possible using button
+  await page.getByRole("textbox").fill("Fifth Task");
+  await page.getByText("Add note").click();
+
+  const task_4 = page.getByText("Fourth task");
+  const task_5 = page.getByText("Fifth Task");
+
+  await expect(task_1).not.toBeVisible();
+  await expect(task_2).not.toBeVisible();
+
+  // task_2 is now selectable as task_3
+  await expect(page.getByText("Third task failed successfully")).toBeVisible();
+  await expect(task_3).toBeVisible();
+
+  await expect(task_4).toBeVisible();
+  await expect(task_5).toBeVisible();
+
+  // edit the "second task again", try assigning with "enter"
+  await task_3.dragTo(task_3, {
+    sourcePosition: { x: 150, y: 0 },
+    targetPosition: { x: 0, y: 0 },
+  });
+  await page.getByRole("dialog").getByRole("textbox").fill("Sixth task");
+  await page.keyboard.press("Enter");
+
+  await task_4.dragTo(task_4, {
+    sourcePosition: { x: 150, y: 0 },
+    targetPosition: { x: 0, y: 0 },
+  });
+  await page
+    .getByRole("dialog")
+    .getByRole("textbox")
+    .fill("Updated 4th task to Seventh Task");
+  await page.keyboard.press("Enter");
+
+  await task_5.dragTo(task_5, {
+    sourcePosition: { x: 0, y: 0 },
+    targetPosition: { x: 150, y: 0 },
+  });
+
+  await expect(page.getByText("Sixth task")).toBeVisible();
+  await expect(
+    page.getByText("Updated 4th task to Seventh Task")
+  ).toBeVisible();
+  await expect(task_5).not.toBeVisible();
+  await expect(task_4).not.toBeVisible();
 });
