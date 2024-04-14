ecaa4191b9e0038f404f86d7d786737ec588d19e
p-w-g <p.grzelczyk@gmail.com>
2024-04-14 20:44:26 +0200

    add rudimentary error handling

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -7,6 +7,8 @@
       <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
       <q-btn label="Sign in" color="secondary" @click="signin" />
     </q-form>
+
+    <p v-if="loginError">{{ loginError }}</p>
   </div>
 </template>
 <script setup>
@@ -22,29 +24,32 @@ const userStore = useUserStore();
 
 const email = ref("");
 const password = ref("");
+const loginError = ref("");
 
 function register() {
-  createUserWithEmailAndPassword(getAuth(), email.value, password.value).then(
-    (data) => {
+  createUserWithEmailAndPassword(getAuth(), email.value, password.value)
+    .then((data) => {
+      loginError.value = "";
       email.value = null;
       password.value = false;
 
       userStore.setUserData(data);
       userStore.toggleIsLoggedIn();
-    }
-  );
+    })
+    .catch((e) => (loginError.value = e));
 }
 
 function signin() {
-  signInWithEmailAndPassword(getAuth(), email.value, password.value).then(
-    (data) => {
+  signInWithEmailAndPassword(getAuth(), email.value, password.value)
+    .then((data) => {
+      loginError.value = "";
       email.value = null;
       password.value = false;
 
       userStore.setUserData(data);
       userStore.toggleIsLoggedIn();
-    }
-  );
+    })
+    .catch((e) => (loginError.value = e));
 }
 
 function resetForm() {
