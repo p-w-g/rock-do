46701f13127ec70a6921ff418a7a5f867e227904
p-w-g <p.grzelczyk@gmail.com>
2024-04-14 17:15:49 +0200

    prep firebase config

--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,17 @@ import { Quasar } from "quasar";
 // @ts-ignore
 import quasarUserOptions from "./quasar-user-options";
 import { createPinia } from "pinia";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_FIREBASE_CONFIG_apiKey,
+  authDomain: process.env.VUE_APP_FIREBASE_CONFIG_authDomain,
+  projectId: process.env.VUE_APP_FIREBASE_CONFIG_projectId,
+  storageBucket: process.env.VUE_APP_FIREBASE_CONFIG_storageBucket,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_CONFIG_messagingSenderId,
+  appId: process.env.VUE_APP_FIREBASE_CONFIG_appId,
+};
+
+initializeApp(firebaseConfig);
 
 createApp(App).use(Quasar, quasarUserOptions).use(createPinia()).mount("#app");
