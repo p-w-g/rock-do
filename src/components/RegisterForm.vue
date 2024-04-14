f9c887c92688688c93b0ed40cae2ecc3aa3ca846
p-w-g <p.grzelczyk@gmail.com>
2024-04-14 17:44:28 +0200

    prepare register form

---
+++ src/components/RegisterForm.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="q-pa-md">
+    <q-form @submit="register" @reset="resetForm" class="q-gutter-md">
+      <q-input v-model.trim="email" label="Email" />
+      <q-input v-model.trim="password" label="Password" />
+      <div>
+        <q-btn label="Register with password" type="submit" color="primary" />
+        <q-btn
+          label="Reset"
+          type="reset"
+          color="primary"
+          flat
+          class="q-ml-sm"
+        />
+      </div>
+    </q-form>
+  </div>
+</template>
+<script setup>
+import { ref } from "vue";
+
+const email = ref("");
+const password = ref("");
+
+function register() {
+  // todo
+}
+function resetForm() {
+  // todo
+}
+</script>
