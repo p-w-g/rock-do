f71c81c954badc6a3b16ed7cb33848d6ae1e754a
p-w-g <p.grzelczyk@gmail.com>
2024-04-14 20:02:11 +0200

    adds signing of existing user

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -3,22 +3,19 @@
     <q-form @submit="register" @reset="resetForm" class="q-gutter-md">
       <q-input v-model.trim="email" label="Email" />
       <q-input v-model.trim="password" label="Password" />
-      <div>
-        <q-btn label="Register with password" type="submit" color="primary" />
-        <q-btn
-          label="Reset"
-          type="reset"
-          color="primary"
-          flat
-          class="q-ml-sm"
-        />
-      </div>
+      <q-btn label="Register with password" type="submit" color="primary" />
+      <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+      <q-btn label="Sign in" color="secondary" @click="signin" />
     </q-form>
   </div>
 </template>
 <script setup>
 import { ref } from "vue";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import { useUserStore } from "../store.ts";
 
 const userStore = useUserStore();
@@ -28,6 +25,18 @@ const password = ref("");
 
 function register() {
   createUserWithEmailAndPassword(getAuth(), email.value, password.value).then(
+    (data) => {
+      email.value = null;
+      password.value = false;
+
+      userStore.setUserData(data);
+      userStore.toggleIsLoggedIn();
+    }
+  );
+}
+
+function signin() {
+  signInWithEmailAndPassword(getAuth(), email.value, password.value).then(
     (data) => {
       email.value = null;
       password.value = false;
